f17ef160540d2d1567281ef6bb7b6a7eefbacd92
BotirovE <erkinboy.botirov@mail.ru>
2018-05-22 13:57:03 +0500

    layout, partial, auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email_or_phone')->unique();
+            $table->string('email')->unique();
+            //$table->string('phone',15)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
